70661e3bc581927c085ece4553948afdb08e5221
Marc Lechuga <marc.lechuga@inserm.fr>
2017-08-31 15:49:54 +0200

    config de sendgrid pour mulitimail

--- app/mailers/seed_mailer.rb
+++ app/mailers/seed_mailer.rb
@@ -1,10 +1,25 @@
 class SeedMailer < ApplicationMailer
+  include SendGrid
   
-  default from: "marc.lechuga@inserm.fr"
+  sendgrid_category :use_subject_lines
+  sendgrid_enable   :ganalytics, :opentrack
+  sendgrid_unique_args :key1 => "value1", :key2 => "value2"
+ 
+  def welcome_message(user)
+    sendgrid_category "Welcome"
+    sendgrid_unique_args :key2 => "newvalue2", :key3 => "value3"
+    mail :to => user.email, :subject => "Welcome #{user.name} :-)"
+  end
   
-  def seed_email(user)
-    @user = user
-    mail :to => "mlechuga@icloud.com", :subject => "Message from Capseed!"
+  def welcome_message(user)
+    sendgrid_category "Welcome"
+    sendgrid_unique_args :key2 => "newvalue2", :key3 => "value3"
+    mail :to => user.email, :subject => "Welcome #{user.name} :-)"
+  end
+
+  def goodbye_message(user)
+    sendgrid_disable :ganalytics
+    mail :to => user.email, :subject => "Fare thee well :-("
   end
   
 end

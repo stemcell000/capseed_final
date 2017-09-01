7d0a09631a9ed8844e780a7e66ca9d4bd3445165
Marc Lechuga <marc.lechuga@inserm.fr>
2017-09-01 13:23:11 +0200

    config sendgrid pour envoi à une liste d'adresses mails suite

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,17 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout 'mailer'
+  include SendGrid
+  sendgrid_category :use_subject_lines
+  sendgrid_enable   :ganalytics, :opentrack
+  sendgrid_unique_args :key1 => "value1", :key2 => "value2"
+
+  def welcome_message(user)
+    sendgrid_category "Welcome"
+    sendgrid_unique_args :key2 => "newvalue2", :key3 => "value3"
+    mail :to => user.email, :subject => "Welcome #{user.name} :-)"
+  end
+
+  def goodbye_message(user)
+    sendgrid_disable :ganalytics
+    mail :to => user.email, :subject => "Fare thee well :-("
+  end
 end

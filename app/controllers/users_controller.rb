9cc02ac0774a656a5ae4887f7c1ce5e1e83183af
Marc Lechuga <marc.lechuga@inserm.fr>
2017-11-08 17:00:51 +0100

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,16 @@ class UsersController < ApplicationController
   
   def inform_cloning
     @user = User.find(1)
-    UserMailer.notify_cloning(@user).deliver_now
+    #UserMailer.notify_cloning(@user).deliver_now
+    UserNotifier.notify_cloning(@user).deliver_now
     redirect_to(assays_path)
     flash.keep[:success] = "Mail sent to cloning users!"
   end
   
   def inform_production
     @user = User.first
-    UserMailer.notify_production(@user).deliver_later
+    #UserMailer.notify_production(@user).deliver_later
+    UserNotifier.notify_production(@user).deliver_later
     redirect_to(productions_path)
     flash.keep[:success] = "Mail sent to production users!"
   end
--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,2 +1,23 @@
 class UserNotifier < ApplicationMailer
+   default :from => 'marc.lechuga@inserm.fr'
+
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Thanks for signing up for our amazing app' )
+  end
+  
+   def notify_cloning(user)   
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Cloning')
+ end
+ 
+  def notify_production(user) 
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Production')
+  end
+  
 end

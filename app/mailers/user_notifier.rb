cf6595843b34fca5adf0bdd6e66f028a013d864a
Marc Lechuga <marc.lechuga@inserm.fr>
2017-11-09 14:49:58 +0100

    wip mailer 4

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -3,24 +3,30 @@ class UserNotifier < ApplicationMailer
 
 
   
-   def notify_cloning(user)   
+  def notify_cloning(user)   
     @user = user
     
-    recipients = User.where(:role => "cloning_user" , :role => "user").pluck(:email)
-    firstnames = User.where(:role => "cloning_user" , :role => "user").pluck(:firstname)
+    recipients = User.where(:role => "cloning_user" ).pluck(:email)
+    firstnames = User.where(:role => "cloning_user" ).pluck(:firstname)
     
     sendgrid_category "Notification"
     
     sendgrid_recipients recipients
     sendgrid_substitute "|subme|", firstnames
     
-    mail :from => "marc.lechuga@inserm.fr", :to => "noreply@address.com", :subject => "Cloning Notification"
+    mail :from => "mailer@capseed.net", :to => "noreply@address.com", :subject => "Cloning Notification"
   end
  
   def notify_production(user) 
-    @user = user
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Production')
+    recipients = User.where(:role => "production_user" ).pluck(:email)
+    firstnames = User.where(:role => "production_user" ).pluck(:firstname)
+    
+    sendgrid_category "Notification"
+    
+    sendgrid_recipients recipients
+    sendgrid_substitute "|subme|", firstnames
+    
+    mail :from => "mailer@capseed.net", :to => "noreply@address.com", :subject => "Cloning Notification"
   end
   
 end

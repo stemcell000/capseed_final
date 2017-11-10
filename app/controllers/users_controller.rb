bcebbe757f4e9b2d41eadf267bec4f7d1a5bb9d7
Marc Lechuga <marc.lechuga@inserm.fr>
2017-11-10 09:25:59 +0100

    wip mailer 5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   
   def inform_cloning
     @user = User.find(1)
-    #UserMailer.notify_cloning(@user).deliver_now
     UserNotifier.notify_cloning(@user).deliver_now
     redirect_to(assays_path)
     flash.keep[:success] = "Mail sent to cloning users!"
@@ -11,8 +10,7 @@ class UsersController < ApplicationController
   
   def inform_production
     @user = User.first
-    #UserMailer.notify_production(@user).deliver_later
-    UserNotifier.notify_production(@user).deliver_later
+    UserNotifier.notify_production(@user).deliver_now
     redirect_to(productions_path)
     flash.keep[:success] = "Mail sent to production users!"
   end

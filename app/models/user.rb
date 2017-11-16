d1f9168ee194be72502d6c8edf635e93176b22d1
Marc Lechuga <marc.lechuga@inserm.fr>
2017-11-16 11:39:35 +0100

    configuartion de Devise pour empêcher sign-up et password recovery

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,8 +22,7 @@ class User < ActiveRecord::Base
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, 
+  devise :database_authenticatable,:rememberable, :trackable, 
          :validatable, :authentication_keys => [:login]
   
   # Virtual attribute for authenticating by either username or email

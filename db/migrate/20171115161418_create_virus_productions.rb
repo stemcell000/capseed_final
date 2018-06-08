7b81c0388532fd267055acd45178b5d3c4e0e848
Marc Lechuga <marc.lechuga@inserm.fr>
2018-06-08 14:18:41 +0200

    titers added

--- db/migrate/20171115161418_create_virus_productions.rb
+++ db/migrate/20171115161418_create_virus_productions.rb
@@ -8,7 +8,7 @@ class CreateVirusProductions < ActiveRecord::Migration
       t.integer :plate_nb
       t.decimal :vol
       t.boolean :sterility
-      t.text :plate_idx
+      t.text :plate_id
       t.text :comment
       t.string :gel_prot
       t.boolean :invoice

dd28c1aedb12c0c8e2ba70bdc38573d764c0e9f2
Marc Lechuga <marc.lechuga@inserm.fr>
2018-12-14 15:50:21 +0100

    msg

--- app/admin/plasmid_batch_production.rb
+++ app/admin/plasmid_batch_production.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register PlasmidBatchProduction do
  active_admin_import validate: false,
               csv_options: {col_sep: ";" },
               before_batch_import: ->(importer) {
-   
+              PlasmidBatchProduction.where(id: importer.values_at('id')).delete_all
               },
               batch_size: 1000
               

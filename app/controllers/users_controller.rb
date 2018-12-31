a155bab6ba0f67c53b7b91a37aab80236aeb3294
Marc Lechuga <marc.lechuga@inserm.fr>
2018-12-31 15:38:37 +0100

    msg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,30 +30,15 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    respond_to do |format|
-      if @user.save(:validate => false)
-        format.html { redirect_to @user }
-        format.json { render :create, status: :created, location: @user }
-        format.js { render :create, status: :created, location: @user }
-        flash[:success] = 'User was successfully created.'
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def update
      @user.update_attributes(user_params)
-     if @user.save
-      flash.keep[:success] = "User successfully updated."
-    else
-      render :action => 'edit'
-    end
   end
 
   def destroy

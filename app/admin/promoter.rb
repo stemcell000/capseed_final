ef3bf9c0acb01f6066cc887402121ba6421c7d6e
Marc Lechuga <marc.lechuga@inserm.fr>
2019-01-07 17:15:33 +0100

    msg

--- app/admin/promoter.rb
+++ app/admin/promoter.rb
@@ -7,20 +7,14 @@ ActiveAdmin.register Promoter do
 
   
 ##Import csv   
- active_admin_import validate: false,
-              csv_options: {col_sep: ";" },
-              headers_rewrites: { 'plasmid' => :clone_batch_id},
-              before_batch_import: ->(importer) {
-                
-                Promoter.where(id: importer.values_at('id')).delete_all
-                
-                clone_batch_names = importer.values_at(:clone_batch_id)
-                clone_batches   = CloneBatch.where(name: clone_batch_names).pluck(:name, :id)
-                options = Hash[*clone_batches.flatten]
-                importer.batch_replace(:clone_batch_id, options)
-                
+active_admin_import validate: true,
+             csv_options: {col_sep: ";" },
+             before_batch_import: ->(importer) {
+               
+               
+
               },
-              batch_size: 1000
+              batch_size: 1000 
 
 
 permit_params :list, :of, :attributes, :on, :model, :id, :name, :clone_batch_id

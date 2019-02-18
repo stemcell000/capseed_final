2ba1767a727b8496a943f3e6a845eb697de82c18
Marc Lechuga <marc.lechuga@inserm.fr>
2019-02-18 15:36:08 +0100

    msg

--- app/admin/clone_batch_production.rb
+++ app/admin/clone_batch_production.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register CloneBatchProduction do
              csv_options: {col_sep: ";" },
              before_batch_import: ->(importer) {
                
-               
+               CloneBatchProduction.where(id: importer.values_at('id')).delete_all
 
               },
               batch_size: 1000 

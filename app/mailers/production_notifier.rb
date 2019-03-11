1b00df91af3119c4ba04f5a1407b1b80645e1929
Marc Lechuga <marc.lechuga@inserm.fr>
2019-03-11 17:05:42 +0100

    msg

--- app/mailers/production_notifier.rb
+++ app/mailers/production_notifier.rb
@@ -4,7 +4,7 @@ class ProductionNotifier < ApplicationMailer
     #TUTO: ligne suivante nécessaire pour passer des variables (attributs de @production) dans le corps du message. 
     @production = production
     recipients = User.where( :production_notify => true, :actual_member => true, :role => :administrator, :firstname => "Marc" ).pluck(:email)
-    firstnames = User.where( :production_notify => true, :actual_member => true, :role => :administrator ).pluck(:firstname)
+    firstnames = User.where( :production_notify => true, :actual_member => true, :role => :administrator, :firstname => "Marc" ).pluck(:firstname)
     sendgrid_category "Notification"
     
     sendgrid_recipients recipients

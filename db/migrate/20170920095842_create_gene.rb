4cfd70204d14f66ab2c93705edd56f0c310d3169
Marc Lechuga <marc.lechuga@inserm.fr>
2019-04-06 00:26:48 +0200

    msg

--- db/migrate/20170920095842_create_gene.rb
+++ db/migrate/20170920095842_create_gene.rb
@@ -1,7 +1,16 @@
 class CreateGene < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :genes do |t|
       t.string :name
     end
-  end
+
+        create_table :clone_batches_genes do |t|
+           t.belongs_to :clone_batch, index: true
+           t.belongs_to :gene, index: true
+        end
+      end
+ 
+      def self.down
+        drop_table :clone_batches_genes
+      end
 end

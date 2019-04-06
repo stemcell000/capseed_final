3d528dcf8070d50b914577124b4d74e1c881a947
Marc Lechuga <marc.lechuga@inserm.fr>
2019-04-06 19:21:44 +0200

    msg

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,39 +2,16 @@
 #CarrierWave.configure do |config|
  # config.storage = :file
  # config.asset_host = ActionController::Base.asset_host
- #end
- ####
- #CarrierWave.configure do |config|
-  #config.fog_provider = 'fog/aws'                        # required
-  #config.fog_credentials = {
-   # provider:              'AWS',                        # required
-   # aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-   # aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-   # host: ENV["AWS_HOST"],
-   # region: ENV["S3_REGION"]
-  #}
-  #@config.fog_directory  = ENV["AWS_BUCKET"]              # required
-  ####
-#end
  CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.fog_credentials = {
-        # Configuration for Amazon S3
-        :provider              => 'AWS',
-        :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['AWS_SECRET_KEY']
-    }
-    config.fog_directory     =  ENV['S3_BUCKET_NAME']
-  else
-    config.fog_credentials = {
-        :provider               => 'AWS',
-        :aws_access_key_id      => '',
-        :aws_secret_access_key  => '',
-        :region                 => 'eu-west-3'
-    }
-    config.storage = :file
-    config.cache_dir = "#{Rails.root}/public/tmp"
-  end
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    host: ENV["AWS_HOST"],
+    region: ENV["S3_REGION"]
+  }
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+
 end
-#end
   

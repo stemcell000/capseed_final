d36409a5e21a663cd8ead8df0cde329eccbf8508
Marc Lechuga <marc.lechuga@inserm.fr>
2019-04-07 11:08:12 +0200

    msg

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV['AWS_BUCKET']
   config.aws_acl    = :public_read
-  config.asset_host = '"https://s3-eu-west-3.amazonaws.com/capseedprod"'
+  config.asset_host = 'https://s3-eu-west-3.amazonaws.com/capseedprod'
   #config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
 

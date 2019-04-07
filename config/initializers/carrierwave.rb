eb194cb1213f548eda6bf9f3192b84ccb8dfc3a9
Marc Lechuga <marc.lechuga@inserm.fr>
2019-04-07 10:47:46 +0200

    msg

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
     region: 'eu-west-3',
-    #host: 's3-eu-west-3.amazonaws.com'
+    host: 's3-eu-west-3.amazonaws.com'
   }
   config.fog_directory  = ENV["AWS_BUCKET"]              # required
 

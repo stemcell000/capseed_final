c18836661383fb1efb315bee6f0cbc5b9af6d242
Marc Lechuga <marc.lechuga@inserm.fr>
2019-04-07 11:02:32 +0200

    msg

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,13 @@
-#Config précédente (upload local seulement)
-#CarrierWave.configure do |config|
- # config.storage = :file
- # config.asset_host = ActionController::Base.asset_host
- CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAYXZCZOJKVTZDNDUD',        # required
-    aws_secret_access_key: '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',        # required
-    region: 'eu-west-3',
-    host: 's3-eu-west-3.amazonaws.com'
-  }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+CarrierWave.configure do |config|
+  config.storage    = :aws
+  config.aws_bucket = ENV['AWS_BUCKET']
+  config.aws_acl    = :public_read
+  config.asset_host = '"https://s3-eu-west-3.amazonaws.com/capseedprod"'
+  #config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
+
+  config.aws_credentials = {
+    access_key_id:     ENV['AWS_ACCESS_KEY'],
+    secret_access_key: ENV['AWS_SECRET_KEY']
+  }
 end
-  

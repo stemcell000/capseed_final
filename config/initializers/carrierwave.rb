3661c3b5bcaa647776805df31b59d1e2ccd2cd14
Marc Lechuga <marc.lechuga@inserm.fr>
2019-04-08 09:46:33 +0200

    msg

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,12 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV['AWS_BUCKET']
-  #config.aws_acl    = :public_read
   config.asset_host = ''
-  #config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
 
   config.aws_credentials = {
     access_key_id:     ENV['AWS_ACCESS_KEY'],
-    secret_access_key: ENV['AWS_SECRET_KEY']
+    secret_access_key: ENV['AWS_SECRET_KEY'],
+    region:            ENV['AWS_REGION']
   }
 end

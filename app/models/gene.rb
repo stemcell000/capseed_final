c5fd7d8d4013b9c43004e7cdf0d8e4271bbfd1ed
Marc Lechuga <marc.lechuga@inserm.fr>
2019-04-15 19:29:17 +0200

    msg

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -1,8 +1,8 @@
 class Gene < ActiveRecord::Base
   
-belongs_to :clone_batch
+has_and_belongs_to_many :clone_batches, :join_table => "clone_batches_genes"
 
-accepts_nested_attributes_for :clone_batch
+accepts_nested_attributes_for :clone_batches
 
   validates :name, :uniqueness => {message: "Unique name, please!"}, :case_sensitive => false
   validates :name, :presence => true

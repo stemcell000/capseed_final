33915a4293ebf1be0e94557fe8e8911021a49bbb
Marc Lechuga <marc.lechuga@inserm.fr>
2019-04-15 18:57:58 +0200

    msg

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190410080021) do
+ActiveRecord::Schema.define(version: 20190415165400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

7b511633f2b66769421f1478dacd228f0a91854b
Marc Lechuga <marc.lechuga@inserm.fr>
2019-04-15 20:15:28 +0200

    msg

--- db/migrate/20170928071524_create_gene_and_promoter.rb
+++
@@ -1,6 +0,0 @@
-class CreateGeneAndPromoter < ActiveRecord::Migration
-     def self.up      
-        add_column :genes, :clone_batch_id, :integer
-        add_column :promoters, :clone_batch_id, :integer
-      end
-end

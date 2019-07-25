30f8486fb92da96ced30f27da26f0c3cbddf06bb
Marc Lechuga <marc.lechuga@inserm.fr>
2019-07-25 10:36:01 +0200

    msg

--- db/migrate/20190723084853_create_options_virus_productions.rb
+++ db/migrate/20190723084853_create_options_virus_productions.rb
@@ -1,7 +1,5 @@
 class CreateOptionsVirusProductions < ActiveRecord::Migration
   def change
-    drop_table :users_virus_productions
-    drop_table :clone_batches_users
     create_table :options_virus_productions do |t|
       t.belongs_to :option, index: true
       t.belongs_to :virus_production, index: true

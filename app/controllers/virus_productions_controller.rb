ed3357ff70d1781d1361b6c84c5a35088a632fc7
Marc Lechuga <marc.lechuga@inserm.fr>
2019-09-06 17:29:27 +0200

    recap process virusproductions

--- app/controllers/virus_productions_controller.rb
+++ app/controllers/virus_productions_controller.rb
@@ -235,7 +235,7 @@ end
       block = block1+block2+block3+block4+block5+block6+block7+block8+block9+block10+block11
       @virus_production.update_columns(:recap => block)
                   
-    end
+    
   end
   
   private

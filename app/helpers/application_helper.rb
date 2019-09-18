776556f40aabfa985e4c224cae9f43864266d601
Marc Lechuga <marc.lechuga@inserm.fr>
2019-09-18 13:49:33 +0200

    correction application helper: methods title et show_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,14 +26,14 @@ module ApplicationHelper
     return myclass
  end 
 
-  def title(page_title, show_title = true)
-    @show_title = show_title
-    @content_for_title = page_title.to_s
-  end
+  #def title(page_title, show_title = true)
+   # @show_title = show_title
+   # @content_for_title = page_title.to_s
+  #end
 
-  def show_title?
-    @show_title
-  end
+  #def show_title?
+   # @show_title
+  #end
   
   def bootstrap_class_for flash_type
     { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
